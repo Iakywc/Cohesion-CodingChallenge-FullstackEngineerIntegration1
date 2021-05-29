a0cfcecd7e1f94ad136ac9d19d8a4a4223cd688b
icoria <ignacio.coria@external.iherb.com>
2021-05-29 13:02:51 -0300

    [DEV] Setup Application DI

--- src/Cohesion.Challenge.CRUD.Api/Startup.cs
+++ src/Cohesion.Challenge.CRUD.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Cohesion.Challenge.CRUD.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Cohesion.Challenge.CRUD.Api
 {
@@ -26,6 +21,9 @@ namespace Cohesion.Challenge.CRUD.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApplication();
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/Cohesion.Challenge.CRUD.Application/InjectionSetup.cs
@@ -0,0 +1,20 @@
+﻿using Cohesion.Challenge.CRUD.Application.Commands;
+using Cohesion.Challenge.CRUD.Application.Queries;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cohesion.Challenge.CRUD.Application
+{
+    public static class InjectionSetup
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddTransient<ICreateServiceRequestCommandHandler, CreateServiceRequestCommandHandler>();
+            services.AddTransient<IDeleteServiceRequestCommandHandler, DeleteServiceRequestCommandHandler>();
+            services.AddTransient<IUpdateServiceRequestCommandHandler, UpdateServiceRequestCommandHandler>();
+            services.AddTransient<IGetAllServiceRequestQueryHandler, GetAllServiceRequestQueryHandler>();
+            services.AddTransient<IGetServiceRequestByIdQueryHandler, GetServiceRequestByIdQueryHandler>();
+
+            return services;
+        }
+    }
+}
