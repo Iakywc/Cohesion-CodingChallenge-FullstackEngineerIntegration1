191fac8a0cee2b8c7d844f132583de734afb6f7c
icoria <ignacio.coria@external.iherb.com>
2021-05-30 14:27:38 -0300

    [DEV] Inject and Map commands and queries to controller

--- src/Cohesion.Challenge.CRUD.Api/Controllers/ServiceRequestController.cs
+++ src/Cohesion.Challenge.CRUD.Api/Controllers/ServiceRequestController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Cohesion.Challenge.CRUD.Api.Contract.CreateServiceRequest;
+using Cohesion.Challenge.CRUD.Api.Extensions;
+using Cohesion.Challenge.CRUD.Application.Commands;
+using Cohesion.Challenge.CRUD.Application.Queries;
 using Cohesion.Challenge.CRUD.Model.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,16 +17,36 @@ namespace Cohesion.Challenge.CRUD.Api.Controllers
     [Route("[controller]")]
     public class ServiceRequestController : ControllerBase
     {
-        public ServiceRequestController()
+        private readonly ICreateServiceRequestCommandHandler _createServiceRequest;
+        private readonly IDeleteServiceRequestCommandHandler _deleteServiceRequest;
+        private readonly IUpdateServiceRequestCommandHandler _updateServiceRequest;
+        private readonly IGetAllServiceRequestQueryHandler _getAllServiceRequests;
+        private readonly IGetServiceRequestByIdQueryHandler _getServiceRequest;
+
+        public ServiceRequestController(ICreateServiceRequestCommandHandler createServiceRequest
+            , IDeleteServiceRequestCommandHandler deleteServiceRequest
+            , IUpdateServiceRequestCommandHandler updateServiceRequest
+            , IGetAllServiceRequestQueryHandler getAllServiceRequest
+            , IGetServiceRequestByIdQueryHandler getServiceRequest)
         {
+            _createServiceRequest = createServiceRequest;
+            _deleteServiceRequest = deleteServiceRequest;
+            _updateServiceRequest = updateServiceRequest;
+            _getAllServiceRequests = getAllServiceRequest;
+            _getServiceRequest = getServiceRequest;
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ServiceRequest>> GetAllServiceRequests(CancellationToken cancellationToken)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<ServiceRequest>>> GetAllServiceRequests(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var allServices = await _getAllServiceRequests.GetAllServiceRequests(cancellationToken);
+
+            if (!allServices.Any())
+                return NoContent();
+
+            return Ok(allServices);
         }
 
         [HttpGet("{id}")]
@@ -29,15 +54,28 @@ namespace Cohesion.Challenge.CRUD.Api.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceRequest>> GetServiceRequestById(string id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //Add Validation - To make sure is a valid GUID
+            var query = id.ToQuery();
+            var service = await _getServiceRequest.GetServiceRequestById(query, cancellationToken);
+
+            if (service == null)
+                return NotFound();
+
+            return Ok(service);
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<ServiceRequest>> PostServiceRequest(CreateUpdateServiceRequestRequest request, CancellationToken cancellationToken)
+        public async Task<ActionResult> PostServiceRequest(CreateUpdateServiceRequestRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //Add Validation - To make all fields are accurate
+            var command = request.ToCommand();
+
+            if (await _createServiceRequest.CreateServiceRequest(command, cancellationToken))
+                return Created("created service request with id", request.Id);
+
+            return BadRequest();
         }
 
         [HttpPut("{id}")]
@@ -46,7 +84,20 @@ namespace Cohesion.Challenge.CRUD.Api.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceRequest>> PutServiceRequestById(string id, CreateUpdateServiceRequestRequest request,  CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //Add Validation - To make all fields are accurate
+                var command = request.ToCommand(id);
+
+                if (await _updateServiceRequest.UpdateServiceRequestById(command, cancellationToken))
+                    return Ok("updated service request");
+                
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
         }
 
         [HttpDelete("{id}")]
@@ -54,7 +105,13 @@ namespace Cohesion.Challenge.CRUD.Api.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceRequest>> DeleteServiceRequestById(string id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //Add Validation - To make all fields are accurate
+            var command = id.ToCommand();
+
+            if (await _deleteServiceRequest.DeleteServiceRequestById(command, cancellationToken))
+                return Ok("successful");
+
+            return NotFound();
         }
     }
 }
---
+++ src/Cohesion.Challenge.CRUD.Api/Extensions/ServiceRequestMappingExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cohesion.Challenge.CRUD.Api.Contract.CreateServiceRequest;
+using Cohesion.Challenge.CRUD.Application.Commands;
+using Cohesion.Challenge.CRUD.Application.Queries;
+using Cohesion.Challenge.CRUD.Model.Enums;
+
+namespace Cohesion.Challenge.CRUD.Api.Extensions
+{
+    public static class ServiceRequestMappingExtension
+    {
+        public static GetServiceRequestByIdQuery ToQuery(this string request)
+        {
+            return new GetServiceRequestByIdQuery(Guid.Parse(request));
+        }
+
+        public static CreateServiceRequestCommand ToCommand(this CreateUpdateServiceRequestRequest request)
+        {
+            return new CreateServiceRequestCommand(Guid.Parse(request.Id)
+                , request.BuildingCode
+                , request.Description
+                , (CurrentStatus)Enum.Parse(typeof(CurrentStatus), request.CurrentStatus)
+                , request.CreatedBy
+                , request.CreatedDate
+                , request.LastModifiedBy
+                , request.LastModifiedDate);
+        }
+
+        public static UpdateServiceRequestCommand ToCommand(this CreateUpdateServiceRequestRequest request, string id)
+        {
+            return new UpdateServiceRequestCommand(Guid.Parse(id)
+                , request.BuildingCode
+                , request.Description
+                , (CurrentStatus) Enum.Parse(typeof(CurrentStatus), request.CurrentStatus)
+                , request.CreatedBy
+                , request.CreatedDate
+                , request.LastModifiedBy
+                , request.LastModifiedDate);
+        }
+
+        public static DeleteServiceRequestCommand ToCommand(this string request)
+        {
+            return new DeleteServiceRequestCommand(Guid.Parse(request));
+        }
+    }
+}
